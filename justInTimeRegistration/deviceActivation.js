64fd2718f60694929d4879086adf4cd0ec8f4fe9
RameezLoladia <rameez.loladia@gmail.com>
2016-08-18 15:59:50 -0700

    Handle asynchronous nature of node.js

--- justInTimeRegistration/deviceActivation.js
+++ justInTimeRegistration/deviceActivation.js
@@ -12,8 +12,7 @@ exports.handler = function(event, context, callback) {
     //Replace it with the AWS region the lambda will be running in
     var region = "us-east-1";
     
-    //Replace it with your AWS account ID
-    var accountId = "XXXXXXXXXXXX";
+    var accountId = event.awsAccountId.toString().trim();
 
     var iot = new AWS.Iot({'region': region, apiVersion: '2015-05-28'});
     var certificateId = event.certificateId.toString().trim();
@@ -67,40 +66,39 @@ exports.handler = function(event, context, callback) {
             return;
         }
         console.log(data);
-
+        
+        /*
+        Step 2) Attach the policy to the certificate
+        */
+        iot.attachPrincipalPolicy({
+            policyName: policyName,
+            principal: certificateARN
+        }, (err, data) => {
+            //Ignore if the policy is already attached
+            if (err && (!err.code || err.code !== 'ResourceAlreadyExistsException')) {
+                console.log(err);
+                callback(err, data);
+                return;
+            }
+            console.log(data);
+            /*
+            Step 3) Activate the certificate. Optionally, you can have your custom Certificate Revocation List (CRL) check
+            logic here and ACTIVATE the certificate only if it is not in the CRL. Revoke the certificate if it is in the CRL
+            */
+            iot.updateCertificate({
+                certificateId: certificateId,
+                newStatus: 'ACTIVE'
+            }, (err, data) => {
+                if (err) {
+                    console.log(err, err.stack); 
+                    callback(err, data);
+                }
+                else {
+                    console.log(data);   
+                    callback(null, "Success, created, attached policy and activated the certificate " + certificateId);
+                }
+            });
+        });
     });
  
-    /*
-    Step 2) Attach the policy to the certificate
-    */
-    iot.attachPrincipalPolicy({
-        policyName: policyName,
-        principal: certificateARN
-    }, (err, data) => {
-        //Ignore if the policy is already attached
-        if (err && (!err.code || err.code !== 'ResourceAlreadyExistsException')) {
-            console.log(err);
-            callback(err, data);
-            return;
-        }
-        console.log(data);
-    });
-    
-    /*
-    Step 3) Activate the certificate. Optionally, you can have your custom Certificate Revocation List (CRL) check
-    logic here and ACTIVATE the certificate only if it is not in the CRL. Revoke the certificate if it is in the CRL
-    */
-    iot.updateCertificate({
-        certificateId: certificateId,
-        newStatus: 'ACTIVE'
-    }, (err, data) => {
-        if (err) {
-            console.log(err, err.stack); 
-            callback(err, data);
-        }
-        else {
-            console.log(data);   
-            callback(null, "Success, created, attached policy and activated the certificate " + certificateId);
-        }
-    });
 }
